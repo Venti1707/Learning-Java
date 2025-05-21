b03058c5f0545619d6d3f7efaa993aebe576a4c1
Venti1707 <nickohse3@gmail.com>
2025-05-21 14:21:15 +0800

    Create Class13_NestedForLoops.java

---
+++ src/package01_basic_concepts/Class13_NestedForLoops.java
@@ -0,0 +1,11 @@
+package package01_basic_concepts;
+
+public class Class13_NestedForLoops {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			for (int j = 1; j <= 5; j++) {
+				System.out.println("i: " + i + ", j: " + j);
+			}
+		}
+	}
+}
