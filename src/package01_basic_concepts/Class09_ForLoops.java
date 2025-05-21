8fdd29e10b95b3bbf67e9c710a04333913e541ea
Venti1707 <nickohse3@gmail.com>
2025-05-21 13:48:49 +0800

    Create Class09_ForLoops.java

---
+++ src/package01_basic_concepts/Class09_ForLoops.java
@@ -0,0 +1,23 @@
+package package01_basic_concepts;
+
+public class Class09_ForLoops {
+
+	public static void main(String[] args) {
+		
+		  //                 ----- Set a condition for executing the code               
+		  //                 |		
+		  //                 |
+		for (int i = 1; i <= 10; i++) {
+		  // |                   |
+		  // |                   ---------------- Execute this if the code has been executed
+		  // |		
+		  // ----- Declare a variable to start the loop
+			
+			System.out.println(i);
+			
+		}
+		
+		
+
+	}
+}
