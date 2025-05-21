7e1b14178753e0b625ddb44e3816b0684c5946b1
Venti1707 <nickohse3@gmail.com>
2025-05-21 11:17:49 +0800

    Create Class06_RelationalOperators.java

---
+++ src/package01_basic_concepts/Class06_RelationalOperators.java
@@ -0,0 +1,38 @@
+package package01_basic_concepts;
+
+public class Class06_RelationalOperators {
+
+	public static void main(String[] args) {
+		int i1 = 355;
+		int i2 = 113;
+		
+		// There are (number) relational operators in Java.
+		// These are:
+		
+		// 1. ==
+		// Equality
+		System.out.println(i1 == i2);
+		
+		// 2. !=
+		// Inequality
+		System.out.println(i1 != i2);
+		
+		// 3. >
+		// Greater than
+		System.out.println(i1 > i2);
+		
+		// 4. <
+		// Lesser than
+		System.out.println(i1 < i2);
+		
+		// 5. >=
+		// Greater than or equal to
+		System.out.println(i1 >= i2);
+		
+		// 6. <=
+		// Lesser than or equal to
+		System.out.println(i1 <= i2);
+
+	}
+
+}
