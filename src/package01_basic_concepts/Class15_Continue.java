4ad1fae2c205922e55e67c028fb44c4369967a6c
Venti1707 <nickohse3@gmail.com>
2025-05-21 14:52:57 +0800

    Create Class15_Continue.java

---
+++ src/package01_basic_concepts/Class15_Continue.java
@@ -0,0 +1,14 @@
+package package01_basic_concepts;
+
+public class Class15_Continue {
+	public static void main(String[] args) {
+		int a = 100;		
+		while (a > 50) {
+			a -= 5;
+			if (a == 75) {
+				continue; // Skip the rest of the loop if the above condition is met
+			}
+			System.out.println(a);
+		}
+	}
+}
