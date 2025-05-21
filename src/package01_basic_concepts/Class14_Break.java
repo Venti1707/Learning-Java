26fe75dbd427b4a6b51ad1964085166d6a367ba4
Venti1707 <nickohse3@gmail.com>
2025-05-21 14:52:56 +0800

    Create Class14_Break.java

---
+++ src/package01_basic_concepts/Class14_Break.java
@@ -0,0 +1,14 @@
+package package01_basic_concepts;
+
+public class Class14_Break {
+	public static void main(String[] args) {
+		int a = 100;		
+		while (a > 50) {
+			a -= 5;
+			if (a == 75) {
+				break; // Stop the loop if the above condition is met
+			}
+			System.out.println(a);
+		}
+	}
+}
