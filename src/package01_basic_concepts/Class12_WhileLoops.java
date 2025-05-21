cf465fe28b0cc728b3b49602c3e12e32dce9a729
Venti1707 <nickohse3@gmail.com>
2025-05-21 14:36:42 +0800

    Create Class12_WhileLoops.java

---
+++ src/package01_basic_concepts/Class12_WhileLoops.java
@@ -0,0 +1,13 @@
+package package01_basic_concepts;
+
+public class Class12_WhileLoops {
+	public static void main(String[] args) {
+		int a = 100;
+			// ----- The code will run as long as this condition is true
+			// |
+		while (a > 50) {
+			a -= 5;
+			System.out.println(a);
+		}
+	}
+}
