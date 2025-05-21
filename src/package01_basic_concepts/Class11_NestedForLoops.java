99bacf7e40fed3cc70925414e636348a4f6b1936
Venti1707 <nickohse3@gmail.com>
2025-05-21 14:23:40 +0800

    Update Class11_NestedForLoops.java

--- src/package01_basic_concepts/Class11_NestedForLoops.java
+++ src/package01_basic_concepts/Class11_NestedForLoops.java
@@ -1,6 +1,6 @@
 package package01_basic_concepts;
 
-public class Class13_NestedForLoops {
+public class Class11_NestedForLoops {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 10; i++) {
 			for (int j = 1; j <= 5; j++) {
