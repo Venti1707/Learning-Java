5bbf959789bbdc52f42f68d2ed558c9b6554e3ac
Venti1707 <nickohse3@gmail.com>
2025-05-21 14:42:23 +0800

    Update Class12_WhileLoops.java

--- src/package01_basic_concepts/Class12_WhileLoops.java
+++ src/package01_basic_concepts/Class12_WhileLoops.java
@@ -2,6 +2,7 @@ package package01_basic_concepts;
 
 public class Class12_WhileLoops {
 	public static void main(String[] args) {
+		// The while loop will only run if the condition is true
 		int a = 100;
 			// ----- The code will run as long as this condition is true
 			// |
