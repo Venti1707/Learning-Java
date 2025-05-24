52223bac8717238e7ff804385e80d9d43818cbd1
Venti1707 <nickohse3@gmail.com>
2025-05-24 20:33:00 +0800

    Create Fibonacci.java

---
+++ src/package00_exercises/folder01_for_loops/Fibonacci.java
@@ -0,0 +1,20 @@
+package package00_exercises.folder01_for_loops;
+
+public class Fibonacci {
+	public static void main(String[] args) {
+		int limit = 18; // Set this number above 2 to find the first (limit + 2) numbers 
+		int num1 = 0;
+		int num2 = 1;
+		int num3;
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		
+		for (int i = 1; i <= limit; i++) {
+			num3 = num2 + num1;
+			System.out.println(num3);
+			num1 = num2;
+			num2 = num3;
+		}
+	}
+}
