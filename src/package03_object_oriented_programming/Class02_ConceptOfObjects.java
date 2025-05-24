b17f6646a50379e7f60c42049fc35b40a1899b01
Venti1707 <nickohse3@gmail.com>
2025-05-24 12:48:02 +0800

    Create Class02_ConceptOfObjects.java

---
+++ src/package03_object_oriented_programming/Class02_ConceptOfObjects.java
@@ -0,0 +1,20 @@
+package package03_object_oriented_programming;
+
+public class Class02_ConceptOfObjects {
+	public static void main(String[] args) {
+		// To create the object of a given class, declare it with respect to the class
+		//                                ----- This is known as the constructor of the class
+		//                                |     Is similar to the method inside the class
+		//    Name of object -----        |     The name must be the same as the class name
+		//                       |        |
+		Class01_ConceptOfClasses c1 = new Class01_ConceptOfClasses();
+		//                            |
+		//                            ----- This new keyword is used to instantiate the object of any class
+	
+		c1.colour = "Green";
+		c1.brand = "Nissan";
+		c1.displayDetails();
+		
+		// We can create multiple objects by repeating the above code, but changing the object name
+	}
+}
