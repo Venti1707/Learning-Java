8770aa0ab28a626c63036e137292bf720a7c8695
Venti1707 <nickohse3@gmail.com>
2025-05-26 22:47:43 +0800

    Create Class08_ConstructorsExplained.java

---
+++ src/package03_object_oriented_programming/Class08_ConstructorsExplained.java
@@ -0,0 +1,9 @@
+package package03_object_oriented_programming;
+
+public class Class08_ConstructorsExplained {
+	// Constructors are a block of code similar to methods
+	// They get executed when an instance of a class is created
+	// They are generally used to initialize the member variables of a class
+	// The name of the constructor must be the same as that of the class name
+	// Constructors do not have any return type and do not return any values
+}
