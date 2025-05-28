ef7aef11e070f0aef8820d2f51b00753786f5658
Venti1707 <nickohse3@gmail.com>
2025-05-28 14:47:28 +0800

    Create Class11_StaticKeyword.java

---
+++ src/package03_object_oriented_programming/Class11_StaticKeyword.java
@@ -0,0 +1,10 @@
+package package03_object_oriented_programming;
+
+public class Class11_StaticKeyword {
+	static String name = "ABC";
+	byte age = 6;
+	
+	public void displayDetails() {
+		System.out.println("Student " + name + " is " + age + " years old");
+	}
+}
