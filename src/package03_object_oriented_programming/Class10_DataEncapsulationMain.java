d48a1bd55afabc3bd14fd9153545d15681c5666e
Venti1707 <nickohse3@gmail.com>
2025-05-28 14:14:53 +0800

    Create Class10_DataEncapsulationMain.java

---
+++ src/package03_object_oriented_programming/Class10_DataEncapsulationMain.java
@@ -0,0 +1,10 @@
+package package03_object_oriented_programming;
+
+public class Class10_DataEncapsulationMain {
+	public static void main(String[] args) {
+		Class09_DataEncapsulation dE1 = new Class09_DataEncapsulation();
+		dE1.setModule("Object Oriented Programming");
+		dE1.setEnrolment(109);
+		dE1.displayDetails();
+	}
+}
