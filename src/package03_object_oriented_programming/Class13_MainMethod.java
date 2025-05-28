0f7b55b002774ec527a8ff90183843a1c8b553e0
Venti1707 <nickohse3@gmail.com>
2025-05-28 15:09:00 +0800

    Create Class13_MainMethod.java

---
+++ src/package03_object_oriented_programming/Class13_MainMethod.java
@@ -0,0 +1,17 @@
+package package03_object_oriented_programming;
+
+public class Class13_MainMethod {
+//                                   ----- Is an array of arguments that can be used when using the command line
+//                                   |
+	public static void main(String[] args) {
+//  |      |      |    |
+//  |      |      |     ----- The entry point of execution for any program
+// 	|	   |	  |	 		  The Java Virtual Machine (JVM) looks for it before starting execution
+// 	|	   |      |	 		  After it is executed, any other methods will be called and executed
+//	|	   |       ----- The main method is not to return any values
+//  |      ----- The object of the class need not be created to be accessed for the JVM
+//	----- Is public so it can be accessed in every project
+// The JVM provides the runtime environment for any Java program
+		// If no main method is found, the NoSuchMethodError:main is thrown
+	}
+}
