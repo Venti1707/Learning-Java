5fda7ac1f970e6ff0297de32f487e518f39bcc14
Venti1707 <nickohse3@gmail.com>
2025-05-30 10:14:34 +0800

    Create Class03_InheritanceMain.java

---
+++ src/package05_inheritance_and_abstraction/Class03_InheritanceMain.java
@@ -0,0 +1,12 @@
+package package05_inheritance_and_abstraction;
+
+public class Class03_InheritanceMain {
+	public static void main(String[] args) {
+		Class02_Inheritance2 c2 = new Class02_Inheritance2();
+		c2.num1 = 17;
+		c2.num2 = 7;
+		
+		c2.addition();
+		c2.subtraction();
+	}
+}
