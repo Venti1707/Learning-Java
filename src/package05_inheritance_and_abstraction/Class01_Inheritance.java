0ba2b1a45e9e547ee387fe761fcbfcd9424bb84d
Venti1707 <nickohse3@gmail.com>
2025-05-30 10:14:16 +0800

    Create Class01_Inheritance.java

---
+++ src/package05_inheritance_and_abstraction/Class01_Inheritance.java
@@ -0,0 +1,10 @@
+package package05_inheritance_and_abstraction;
+
+public class Class01_Inheritance {
+	int num1;
+	int num2;
+	
+	public void addition() {
+		System.out.println("num1 + num2 = " + (num1 + num2));
+	}
+}
