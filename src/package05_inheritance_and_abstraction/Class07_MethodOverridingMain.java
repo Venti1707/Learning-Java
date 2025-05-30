5d9315625ecb7d8321919bbfb6779e3abf55fe75
Venti1707 <nickohse3@gmail.com>
2025-05-30 22:00:08 +0800

    Create Class07_MethodOverridingMain.java

---
+++ src/package05_inheritance_and_abstraction/Class07_MethodOverridingMain.java
@@ -0,0 +1,15 @@
+package package05_inheritance_and_abstraction;
+
+public class Class07_MethodOverridingMain {
+	public static void main(String[] args) {
+		Class04_MethodOverriding c1 = new Class04_MethodOverriding();
+		c1.whoAmI();
+		
+		Class05_MethodOverriding2 c2 = new Class05_MethodOverriding2();
+		c2.whoAmI(); // The method from Class05 overrides the method in Class04
+		
+		Class06_MethodOverriding3 c3 = new Class06_MethodOverriding3();
+		c3.whoAmI(); // The method from Class0 overrides the method in Class04
+	
+	}
+}
