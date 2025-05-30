cb41406c823ccffa181791b4c8be5e881f38acba
Venti1707 <nickohse3@gmail.com>
2025-05-30 16:27:22 +0800

    Create Polygon.java

---
+++ src/package00_exercises/folder05_inheritance/Polygon.java
@@ -0,0 +1,11 @@
+package package00_exercises.folder05_inheritance;
+
+public class Polygon {
+	double base;
+	double height;
+	
+	public void setBaseAndHeight(double base, double height) {
+		this.base = base;
+		this.height = height;
+	}
+}
