538658d04c1ed49b122514f5a91936928836cc9e
Venti1707 <nickohse3@gmail.com>
2025-05-30 22:00:05 +0800

    Create Class05_MethodOverriding2.java

---
+++ src/package05_inheritance_and_abstraction/Class05_MethodOverriding2.java
@@ -0,0 +1,7 @@
+package package05_inheritance_and_abstraction;
+
+public class Class05_MethodOverriding2 extends Class04_MethodOverriding {
+	public void whoAmI() {
+		System.out.println("I am from Class 05");
+	}
+}
