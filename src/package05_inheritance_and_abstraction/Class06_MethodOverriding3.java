6daa9969446bbdd1bca9245aa8a5af4b18a86886
Venti1707 <nickohse3@gmail.com>
2025-05-31 12:51:39 +0800

    Update Class06_MethodOverriding3.java

--- src/package05_inheritance_and_abstraction/Class06_MethodOverriding3.java
+++ src/package05_inheritance_and_abstraction/Class06_MethodOverriding3.java
@@ -1,6 +1,6 @@
 package package05_inheritance_and_abstraction;
 
-public class Class06_MethodOverriding3 {
+public class Class06_MethodOverriding3 extends Class04_MethodOverriding {
 	public void whoAmI() {
 		System.out.println("I am from Class 06");
 	}
