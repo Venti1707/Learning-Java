02c23eab005afd00a8c42ec3798b576bd34b9251
Venti1707 <nickohse3@gmail.com>
2025-06-02 13:42:00 +0800

    Create Class11_SuperKeyword3.java

---
+++ src/package05_inheritance_and_abstraction/Class11_SuperKeyword3.java
@@ -0,0 +1,8 @@
+package package05_inheritance_and_abstraction;
+
+public class Class11_SuperKeyword3 {
+	public static void main(String[] args) {
+		Class10_SuperKeyword2 sK2 = new Class10_SuperKeyword2();
+		sK2.doThis2();
+	}
+}
