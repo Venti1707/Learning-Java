0e82623581a66160b7672ec1976f95f159a0a625
Venti1707 <nickohse3@gmail.com>
2025-06-02 13:41:58 +0800

    Create Class09_SuperKeyword.java

---
+++ src/package05_inheritance_and_abstraction/Class09_SuperKeyword.java
@@ -0,0 +1,8 @@
+package package05_inheritance_and_abstraction;
+
+public class Class09_SuperKeyword {
+	int num1 = 9;
+	public void doThis() {
+		System.out.println("doThis() from Class 09 called");
+	}
+}
